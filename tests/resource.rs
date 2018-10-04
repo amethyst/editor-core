a9605f462578bde28adeeccbf9cec77177e87ea4
David LeGare <excaliburhissheath@gmail.com>
2018-10-04 17:42:44 -0500

    Add resources tests

---
+++ tests/resource.rs
@@ -0,0 +1,78 @@
+extern crate amethyst;
+extern crate amethyst_editor_sync;
+#[macro_use]
+extern crate serde;
+
+use amethyst::prelude::*;
+use amethyst_editor_sync::*;
+
+#[derive(Debug, Clone, Copy, Serialize)]
+struct SimpleResource {
+    value: usize,
+}
+
+#[test]
+fn serialize_resource() -> amethyst::Result<()> {
+    #[derive(Debug, Clone, Copy, Default)]
+    struct TestState {
+        frames: usize,
+    };
+
+    impl<'a, 'b> State<GameData<'a, 'b>> for TestState {
+        fn on_start(&mut self, data: StateData<GameData>) {
+            data.world.add_resource(SimpleResource {
+                value: 123,
+            });
+        }
+
+        fn update(&mut self, _: StateData<GameData>) -> Trans<GameData<'a, 'b>> {
+            self.frames += 1;
+            if self.frames > 10 {
+                Trans::Quit
+            } else {
+                Trans::None
+            }
+        }
+    }
+
+    let editor_sync_bundle = SyncEditorBundle::new()
+        .sync_resource::<SimpleResource>("Test State");
+    let game_data = GameDataBuilder::default()
+        .with_bundle(editor_sync_bundle)?;
+    let mut game = Application::build(".", TestState::default())?
+        .build(game_data)?;
+
+    game.run();
+
+    Ok(())
+}
+
+#[test]
+fn missing_resource() -> amethyst::Result<()> {
+    #[derive(Debug, Clone, Copy, Default)]
+    struct TestState {
+        frames: usize,
+    };
+
+    impl<'a, 'b> State<GameData<'a, 'b>> for TestState {
+        fn update(&mut self, _: StateData<GameData>) -> Trans<GameData<'a, 'b>> {
+            self.frames += 1;
+            if self.frames > 10 {
+                Trans::Quit
+            } else {
+                Trans::None
+            }
+        }
+    }
+
+    let editor_sync_bundle = SyncEditorBundle::new()
+        .sync_resource::<SimpleResource>("Test State");
+    let game_data = GameDataBuilder::default()
+        .with_bundle(editor_sync_bundle)?;
+    let mut game = Application::build(".", TestState::default())?
+        .build(game_data)?;
+
+    game.run();
+
+    Ok(())
+}
